ed51751aa55cf309351840e25b5e4eb5fe435124
JauniusPinelis <JauniusPinelis@gmail.com>
2021-06-11 18:19:09 +0300

    First project

---
+++ MyFirstConsoleAppSolution/MyFirstConsoleAppProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstConsoleAppProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
