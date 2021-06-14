d35a689cc54867a60b43129f54fa4dd3dc97e672
JauniusPinelis <JauniusPinelis@gmail.com>
2021-06-14 20:26:01 +0300

    Updated with while

--- MyFirstConsoleAppSolution/MyFirstConsoleAppProject/Program.cs
+++ MyFirstConsoleAppSolution/MyFirstConsoleAppProject/Program.cs
@@ -7,20 +7,24 @@ namespace MyFirstConsoleAppProject
     {
         static void Main(string[] args)
         {
-           
+            string command = "";
 
-            InputProcessor inputProcessor = new InputProcessor();
-            Calculator calculator = new Calculator();
+            while (command != "Exit")
+            {
+                InputProcessor inputProcessor = new InputProcessor();
+                Calculator calculator = new Calculator();
 
-            Console.WriteLine("Enter expression");
-            var consoleInput = Console.ReadLine(); // 5 + 5
+                Console.WriteLine("Enter expression");
+                var consoleInput = Console.ReadLine(); // 5 + 5
 
-           
-            InputModel parsedInput = inputProcessor.ProcessInput(consoleInput);
+                InputModel parsedInput = inputProcessor.ProcessInput(consoleInput);
 
-            var number = calculator.ProcessCalculation(parsedInput);
+                var number = calculator.ProcessCalculation(parsedInput);
 
-            Console.WriteLine(number);
+                Console.WriteLine(number);
+                Console.WriteLine("Type 'Exit' if you want to exit or any button to continue");
+                command = Console.ReadLine();
+            }
         }
     }
 }
