2b976f2be69f1d84d07fdb908e6aefbe48b03c04
JauniusPinelis <JauniusPinelis@gmail.com>
2021-06-23 20:22:38 +0300

    Updated requirements

--- ShopApplication.Solution/ShopApplication.Project/Program.cs
+++ ShopApplication.Solution/ShopApplication.Project/Program.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace ShopApplication.Project
+﻿namespace ShopApplication.Project
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
