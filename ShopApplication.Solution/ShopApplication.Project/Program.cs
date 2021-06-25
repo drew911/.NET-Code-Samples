bee19649b36d17a2cb6b6f27a4d7beb4f05556aa
JauniusPinelis <JauniusPinelis@gmail.com>
2021-06-25 18:47:34 +0300

    Created basic shop

--- ShopApplication.Solution/ShopApplication.Project/Program.cs
+++ ShopApplication.Solution/ShopApplication.Project/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace ShopApplication.Project
+﻿using System;
+
+namespace ShopApplication.Project
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            Shop shop = new Shop();
 
+            string command = "";
+
+            Console.WriteLine("Possible commands: Exit, Buy");
+            while (command != "Exit")
+            {
+                command = Console.ReadLine();
+
+                if (command.StartsWith("Buy"))
+                {
+                    string itemName = command.Split(" ")[1];
+                    shop.Buy(itemName);
+                }
+            }
         }
     }
 }
