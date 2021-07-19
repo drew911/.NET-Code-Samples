8e634e9214b977661887ca3fd5b374d09fa4f877
JauniusPinelis <JauniusPinelis@gmail.com>
2021-07-19 21:00:35 +0300

    Finished RazorNames

---
+++ RazorNamesApplication/RazorNamesApplication/Controllers/NameController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RazorNamesApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorNamesApplication.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitName(NameModel model)
+        {
+            // REading 
+            string readModels = System.IO.File.ReadAllText("Names.json");
+            List<NameModel> receivedModels = JsonConvert.DeserializeObject<List<NameModel>>(readModels);
+
+            if (receivedModels == null)
+            {
+                receivedModels = new();
+            }
+            // Updating the models
+            receivedModels.Add(model);
+            // Saving the updated
+
+            string jsonModels = JsonConvert.SerializeObject(receivedModels);
+
+            System.IO.File.WriteAllText("Names.json", jsonModels);
+
+            return View("NamesDisplay", receivedModels);
+        }
+    }
+}
