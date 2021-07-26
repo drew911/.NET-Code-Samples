01e75470c0a12b411b520e8cff3609a9b56b0fa8
JauniusPinelis <JauniusPinelis@gmail.com>
2021-07-26 18:30:45 +0300

    Refactoring

--- SavedTodoList/SavedTodoList/Controllers/TodoController.cs
+++ SavedTodoList/SavedTodoList/Controllers/TodoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SavedTodoList.Models;
+using SavedTodoList.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,19 @@ namespace SavedTodoList.Controllers
 {
     public class TodoController : Controller
     {
+        private JsonFileService _jsonFileService;
+
+        public TodoController()
+        {
+            _jsonFileService = new JsonFileService();
+        }
+
         public IActionResult TodoList()
         {
-            string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
-            List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+            //string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
+            //List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+            
+            List<TodoItem> items = _jsonFileService.GetItemsFromFile();
 
             return View(items);
         }
@@ -24,29 +34,34 @@ namespace SavedTodoList.Controllers
             return View();
         }
 
-        public IActionResult ToggleTodo(string id, bool value)
+        public IActionResult ToggleTodo(string id)
         {
-            string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
-            List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+            //string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
+            //List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+
+            List<TodoItem> items = _jsonFileService.GetItemsFromFile();
 
             TodoItem selectedItem = items.FirstOrDefault(i => i.Id == id);
             selectedItem.Completed = !selectedItem.Completed;
 
-            itemsText = JsonConvert.SerializeObject(items);
-            System.IO.File.WriteAllText("./Data/Data.json", itemsText);
+            //itemsText = JsonConvert.SerializeObject(items);
+            //System.IO.File.WriteAllText("./Data/Data.json", itemsText);
+
+            _jsonFileService.WriteItemsToFile(items);
 
             return Ok();
         }
 
         public IActionResult SubmitTodo(TodoItem todoItem)
         {
-            if (!System.IO.File.Exists("./Data/Data.json"))
-            {
-                System.IO.File.WriteAllText("./Data/Data.json", "[]");
-            }
+            //if (!System.IO.File.Exists("./Data/Data.json"))
+            //{
+            //    System.IO.File.WriteAllText("./Data/Data.json", "[]");
+            //}
 
-            string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
-            List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+            //string itemsText = System.IO.File.ReadAllText("./Data/Data.json");
+            //List<TodoItem> items = JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+            List<TodoItem> items = _jsonFileService.GetItemsFromFile();
 
             if (items == null)
             {
@@ -55,8 +70,10 @@ namespace SavedTodoList.Controllers
 
             items.Add(todoItem);
 
-            itemsText = JsonConvert.SerializeObject(items);
-            System.IO.File.WriteAllText("./Data/Data.json", itemsText);
+            //itemsText = JsonConvert.SerializeObject(items);
+            //System.IO.File.WriteAllText("./Data/Data.json", itemsText);
+
+            _jsonFileService.WriteItemsToFile(items);
 
             return RedirectToAction("TodoList");
         }
---
+++ SavedTodoList/SavedTodoList/Services/JsonFileService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using SavedTodoList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavedTodoList.Services
+{
+    public class JsonFileService
+    {
+        private const string DATA_URL = "./Data/Data.json";
+
+        public List<TodoItem> GetItemsFromFile()
+        {
+            if (!System.IO.File.Exists(DATA_URL))
+            {
+                System.IO.File.WriteAllText(DATA_URL, "[]");
+            }
+
+            string itemsText = System.IO.File.ReadAllText(DATA_URL);
+            return JsonConvert.DeserializeObject<List<TodoItem>>(itemsText);
+        }
+
+        public void WriteItemsToFile(List<TodoItem> items)
+        {
+            string itemsText = JsonConvert.SerializeObject(items);
+            System.IO.File.WriteAllText(DATA_URL, itemsText);
+        }
+    }
+}
